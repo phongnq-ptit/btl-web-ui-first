44d338dfb0121de070aaecb588641459e03fead6
phongnq-ptit <ngphonga4@gmail.com>
2022-12-26 15:58:52 +0700

    update code

--- src/components/BillBookItem.tsx
+++ src/components/BillBookItem.tsx
@@ -1,10 +1,12 @@
 import {
+  Box,
   Collapse,
   Grid,
   List,
   ListItemButton,
   ListItemText,
   ListSubheader,
+  Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import ExpandLess from "@mui/icons-material/ExpandLess";
@@ -16,7 +18,7 @@ interface Props {
 }
 
 const BillBookItem = ({ props }: { props: Props }) => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(true);
 
   const handleClick = () => {
     setOpen(!open);
@@ -34,68 +36,113 @@ const BillBookItem = ({ props }: { props: Props }) => {
 
   return (
     <React.Fragment>
-      <ListItemButton onClick={handleClick}>
+      <ListItemButton
+        onClick={handleClick}
+        sx={{ backgroundColor: "#d7d7d7", borderBottom: "3px solid #ccc" }}
+      >
         <ListItemText primary={"Tên sách: " + props.cartItem.books.title} />
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListSubheader>Thông tin sách:</ListSubheader>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              <Grid
-                container
-                spacing={2}
-                direction="column"
-                sx={{ color: "darkgreen", fontWeight: 500 }}
+          <Grid container spacing={2} mb={2}>
+            <Grid item xs={6}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
               >
-                <Grid item xs={1}>
-                  Tên Sách:
+                <Box
+                  component="img"
+                  sx={{
+                    width: "300px",
+                    height: "300px",
+                    objectFit: "contain",
+                    p: 1,
+                    backgroundColor: "#c5dcff",
+                    borderRadius: "5px",
+                  }}
+                  src={props.cartItem.books.image?.url}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Grid container spacing={2}>
+                <Grid item xs={3}>
+                  <Grid
+                    container
+                    spacing={2}
+                    direction="column"
+                    sx={{ color: "darkgreen", fontWeight: 500 }}
+                  >
+                    <Grid item xs={1}>
+                      Tên Sách:
+                    </Grid>
+                    <Grid item xs={1}>
+                      Tác Giả:
+                    </Grid>
+                    <Grid item xs={1}>
+                      Ngày Xuất Bản:
+                    </Grid>
+                    <Grid item xs={1}>
+                      Số Trang:
+                    </Grid>
+                    <Grid item xs={1}>
+                      Thể Loại:
+                    </Grid>
+                    <Grid item xs={1}>
+                      Giá Tiền:
+                    </Grid>
+                    <Grid item xs={6}>
+                      Mô Tả:
+                    </Grid>
+                  </Grid>
                 </Grid>
-                <Grid item xs={1}>
-                  Tác Giả:
-                </Grid>
-                <Grid item xs={1}>
-                  Ngày Xuất Bản:
-                </Grid>
-                <Grid item xs={1}>
-                  Số Trang:
-                </Grid>
-                <Grid item xs={1}>
-                  Thể Loại:
-                </Grid>
-                <Grid item xs={1}>
-                  Giá Tiền:
-                </Grid>
-                <Grid item xs={6}>
-                  Mô Tả:
+                <Grid item xs={9}>
+                  <Grid container spacing={2} direction="column">
+                    <Grid item xs={1}>
+                      {props.cartItem.books.title}
+                    </Grid>
+                    <Grid item xs={1}>
+                      {props.cartItem.books.author}
+                    </Grid>
+                    <Grid item xs={1}>
+                      {props.cartItem.books.date}
+                    </Grid>
+                    <Grid item xs={1}>
+                      {props.cartItem.books.page}
+                    </Grid>
+                    <Grid item xs={1}>
+                      {props.cartItem.books.category.label}
+                    </Grid>
+                    <Grid item xs={1}>
+                      {formatter.format(props.cartItem.books.price!)}
+                    </Grid>
+                    <Grid item xs={6}>
+                      {props.cartItem.books.description}
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item xs={9}>
-              <Grid container spacing={2} direction="column">
-                <Grid item xs={1}>
-                  {props.cartItem.books.title}
-                </Grid>
-                <Grid item xs={1}>
-                  {props.cartItem.books.author}
-                </Grid>
-                <Grid item xs={1}>
-                  {props.cartItem.books.date}
-                </Grid>
-                <Grid item xs={1}>
-                  {props.cartItem.books.page}
-                </Grid>
-                <Grid item xs={1}>
-                  {props.cartItem.books.category.label}
-                </Grid>
-                <Grid item xs={1}>
-                  {formatter.format(props.cartItem.books.price!)}
-                </Grid>
-                <Grid item xs={6}>
-                  {props.cartItem.books.description}
-                </Grid>
-              </Grid>
+          </Grid>
+          <ListSubheader>Thông tin mua hàng:</ListSubheader>
+          <Grid container spacing={2} mb={2}>
+            <Grid item xs={3}>
+              Số lượng:
+              <Typography variant="h6" color="#34aadc" mt={1}>
+                {props.cartItem.quantity}
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              Thành tiền:
+              <Typography variant="h6" color="#34aadc" mt={1}>
+                {totalPrice}
+              </Typography>
             </Grid>
           </Grid>
         </List>
--- src/screens/common/BillDetail.tsx
+++ src/screens/common/BillDetail.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  Chip,
   Grid,
   List,
   ListSubheader,
@@ -9,10 +10,11 @@ import {
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Bill, User } from "../../interface";
+import { Bill, BillStatus, User } from "../../interface";
 import useBillApi from "../../hooks/useBillApi";
-import { errorNotify } from "../../Notification";
+import { errorNotify, successNotify } from "../../Notification";
 import BillBookItem from "../../components/BillBookItem";
+import StyledDialog from "../../components/StyledDialog";
 
 const BillDetail = () => {
   const params = useParams();
@@ -23,9 +25,27 @@ const BillDetail = () => {
     minimumFractionDigits: 0,
   });
 
-  const { getBill } = useBillApi();
+  const { getBill, updateBillStatus } = useBillApi();
 
   const [bill, setBill] = useState<Bill>();
+  const [totalBill, setTotalBill] = useState<number>(0);
+  const [reload, setReload] = useState<boolean>(false);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+
+  const handleUpdateStatus = (status: BillStatus) => {
+    updateBillStatus(bill?.id!, status)
+      .then((response) => {
+        if (response.data !== null) {
+          successNotify(`Thay đổi trạng thái đơn hàng thành công!`);
+          setReload(!reload);
+        } else {
+          errorNotify(response.message);
+        }
+      })
+      .finally(() => {
+        setOpenDialog(false);
+      });
+  };
 
   useEffect(() => {
     if (params.id) {
@@ -33,60 +53,118 @@ const BillDetail = () => {
         .then((response) => {
           if (response.data !== null) {
             setBill(response.data);
+            setTotalBill(
+              response.data.listProducts.reduce((sum, currVal) => {
+                return sum + currVal.books.price * currVal.quantity;
+              }, 0)
+            );
           } else {
+            setTotalBill(0);
             errorNotify(response.message);
           }
         })
         .catch((e) => errorNotify(e.message));
     }
     // eslint-disable-next-line
-  }, [params.id]);
+  }, [params.id, reload]);
+
+  const colorChip = () => {
+    switch (bill?.status) {
+      case BillStatus.PENDING:
+        return "error";
+      case BillStatus.COMPLETED:
+        return "success";
+      case BillStatus.CANCEL:
+        return "warning";
+      default:
+        return "info";
+    }
+  };
 
   return (
-    <Box sx={{ width: "85%", margin: "0 auto", pt: 12 }}>
-      <Link to="/">
-        <Button>
-          <ArrowBackIcon sx={{ fontSize: "40px", fontWeight: 700 }} />
-        </Button>
-      </Link>
-      <List>
-        <Box>
-          <ListSubheader>Thông tin người mua:</ListSubheader>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              Tên người nhận:
-              <Typography variant="h6" color="#34aadc" mt={1}>
-                {bill?.info.name}
-              </Typography>
+    <React.Fragment>
+      <Box sx={{ width: "85%", margin: "0 auto", pt: 12 }}>
+        <Link to="/">
+          <Button>
+            <ArrowBackIcon sx={{ fontSize: "40px", fontWeight: 700 }} />
+          </Button>
+        </Link>
+        <List>
+          <Box>
+            <ListSubheader>Thông tin người mua:</ListSubheader>
+            <Grid container spacing={2}>
+              <Grid item xs={2}>
+                Tên người nhận:
+                <Typography variant="h6" color="#34aadc" mt={1}>
+                  {bill?.info.name}
+                </Typography>
+              </Grid>
+              <Grid item xs={2}>
+                Email người nhận:
+                <Typography variant="h6" color="#34aadc" mt={1}>
+                  {bill?.info.email}
+                </Typography>
+              </Grid>
+              <Grid item xs={2}>
+                Số điện thoại:
+                <Typography variant="h6" color="#34aadc" mt={1}>
+                  {bill?.info.tel}
+                </Typography>
+              </Grid>
+              <Grid item xs={2}>
+                Địa chỉ nhận:
+                <Typography variant="h6" color="#34aadc" mt={1}>
+                  {bill?.info.address}
+                </Typography>
+              </Grid>
+              <Grid item xs={2}>
+                Tổng giá trị đơn hàng:
+                <Typography variant="h6" color="#34aadc" mt={1}>
+                  {formatter.format(totalBill)}
+                </Typography>
+              </Grid>
+              <Grid item xs={2}>
+                Trạng thái:
+                <Box mt={1}>
+                  <Chip
+                    label={bill?.status}
+                    size="small"
+                    color={colorChip()}
+                    variant="outlined"
+                  />
+                </Box>
+              </Grid>
             </Grid>
-            <Grid item xs={3}>
-              Email người nhận:
-              <Typography variant="h6" color="#34aadc" mt={1}>
-                {bill?.info.email}
-              </Typography>
-            </Grid>
-            <Grid item xs={3}>
-              Số điện thoại:
-              <Typography variant="h6" color="#34aadc" mt={1}>
-                {bill?.info.tel}
-              </Typography>
-            </Grid>
-            <Grid item xs={3}>
-              Địa chỉ nhận:
-              <Typography variant="h6" color="#34aadc" mt={1}>
-                {bill?.info.address}
-              </Typography>
-            </Grid>
-          </Grid>
-          <ListSubheader>Thông tin sản phẩm đã mua:</ListSubheader>
-          <React.Fragment>
-            {bill?.listProducts.map((item) => (
-              <BillBookItem key={item.id} props={{ cartItem: item }} />
-            ))}
-          </React.Fragment>
-        </Box>
-      </List>
-    </Box>
+            <ListSubheader>Thông tin sản phẩm đã mua:</ListSubheader>
+            <React.Fragment>
+              {bill?.listProducts.map((item) => (
+                <BillBookItem key={item.id} props={{ cartItem: item }} />
+              ))}
+            </React.Fragment>
+            <Box my={2} display="flex" justifyContent="center">
+              {bill?.status === BillStatus.PENDING && (
+                <Button
+                  variant="outlined"
+                  color={colorChip()}
+                  onClick={() => setOpenDialog(true)}
+                >
+                  Hủy đơn hàng
+                </Button>
+              )}
+            </Box>
+          </Box>
+        </List>
+      </Box>
+      <StyledDialog
+        props={{
+          open: openDialog,
+          setOpen: setOpenDialog,
+          title: "Hãy xác nhận",
+          content: "Bạn có muốn thay đổi trạng thái đơn hàng?",
+          handleClickOk: () => handleUpdateStatus(BillStatus.CANCEL),
+        }}
+      />
+    </React.Fragment>
   );
 };
 
